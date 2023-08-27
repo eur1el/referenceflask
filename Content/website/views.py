53fd524c0973808619c2f3a88b438f1d34f2d4ff
eur1el <17105@my.sanctamaria.school.nz>
2023-08-28 01:25:58 +1200

    finalised version

--- Content/website/views.py
+++ Content/website/views.py
@@ -199,7 +199,7 @@ def save_picture(form_picture):
     path = Path(current_app.root_path) / 'static' / 'profile_pics'
 
     # Generate a random filename using tokens
-    random_hex = token_hex(8)
+    random_hex = random_tokenhex(8)
     _,f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
 
